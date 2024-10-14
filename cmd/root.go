f345a0ac22bee05247f1b7753e6b37bf8e6ab17a
adolci <141414129+cipodrew@users.noreply.github.com>
2024-10-14 18:13:07 +0200

    working Hello World Cobra application

---
+++ cmd/root.go
@@ -0,0 +1,24 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"os"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "todo",
+	Short: "todo is a task manager",
+	Long:  `command line todo application`,
+	Run: func(cmd *cobra.Command, args []string) {
+		// Do Stuff Here
+		fmt.Printf("Hello World!")
+	},
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "go-crud/cmd"
+
+func main() {
+	cmd.Execute()
+}
